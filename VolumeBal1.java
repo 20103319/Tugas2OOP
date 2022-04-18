3417de1cc20e5c798304c014675550f03e80de71
20103319 <101190963+20103319@users.noreply.github.com>
2022-04-18 19:36:42 +0800

    menghitung volume balok method 2

---
+++ VolumeBal1.java
@@ -0,0 +1,20 @@
+
+package volumebal1;
+
+/**
+ *
+ * @author Marhaini Hidayati_20103319
+ */
+public class VolumeBal1 {
+
+   
+    public static void main(String[] args) {
+        HVolumebal1 balok = new HVolumebal1(0,0 ,0 );
+        balok.setLebar(7);
+        balok.setPanjang(10);
+        balok.setTinggi(15);
+        balok.tapvol();
+    
+    }
+    
+}
