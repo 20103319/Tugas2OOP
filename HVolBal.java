b12d1208898941a56706d4f658ca8499edc5304f
20103319 <101190963+20103319@users.noreply.github.com>
2022-04-18 19:34:45 +0800

    menghitung volume balok method 1.

---
+++ HVolBal.java
@@ -0,0 +1,63 @@
+
+package volbal;
+
+/**
+ *
+ * @author marhaini hidayati_20103319
+ */
+public class HVolBal {
+   public double pj, lb, tg, vol;
+    
+    public HVolBal (double Pj, double Lb, double Tg){
+        
+        this.setPanjang(Pj);
+        this.setLebar(Lb);
+        this.setTinggi(Tg);
+        
+    }
+    
+   private void setPanjang(double Pj){
+        
+        this.pj=Pj;
+        
+    }
+    
+    private void setLebar(double Lb){
+        
+        this.lb=Lb;
+        
+    }
+    
+    private void setTinggi (double Tg){
+        
+        this.tg=Tg;
+        
+    }
+    
+    public double HV(){
+        
+        this.vol= this.pj * this.lb * this.tg; 
+        return this.vol;
+        
+    }
+    
+    public void tapvol(){
+        
+        this.HV();
+        System.out.println("Hitung Volume Balok");
+        System.out.println("======================");
+        System.out.println("Nilai Panjang = "+this.pj+"cm²");
+        System.out.println("Nilai Lebar = "+this.lb+"cm²");
+        System.out.println("Nilai Tinggi = "+this.tg+"cm²");
+        System.out.println("Nilai Volume Balok = "+this.vol+"cm³");
+        
+    }
+    
+    
+}
+
+        
+    
+    
+
+    
---
+++ VolBal.java
@@ -0,0 +1,16 @@
+
+package volbal;
+
+/**
+ *
+ * @author Marhaini Hidayati_20103319
+ */
+public class VolBal {
+
+    
+    public static void main(String[] args) {
+        HVolBal balok = new HVolBal(3,6,12 );
+        balok.tapvol();
+    }
+    
+}
